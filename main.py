7f7935097152d9d3c4d50ade48eef444bc40042c
Fidel <fidel@flm.cat>
2025-07-05 18:39:43 +0200

    More things

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import PySide6.QtCore
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtCore import Slot
 
 
-print(PySide6.__version__)
-print(PySide6.QtCore.__version__)
+@Slot()
+def say_hello():
+    print("Button clicked, Hello, World!")
+
+
+app = QApplication(sys.argv)
+button = QPushButton("Click Me")
+button.clicked.connect(say_hello)
+
+button.show()
+app.exec()
